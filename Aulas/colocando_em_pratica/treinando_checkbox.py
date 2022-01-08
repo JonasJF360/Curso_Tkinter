bc36175ee72e5025e56aabe3a91c59bbed58295f
JonasJF360 <jonasjf360@gmail.com>
2022-01-08 17:52:52 -0400

    Atualizaçao de codigo

--- Aulas/colocando_em_pratica/treinando_checkbox.py
+++ Aulas/colocando_em_pratica/treinando_checkbox.py
@@ -5,26 +5,17 @@ paises = [
     'Afeganistão', 'Albania', 'Algeria', 'Andorra',
     'Angola', 'Australia', 'Bolivia', 'Brasil',
     'Canada', 'China', 'Clile', 'Congo', 'Mexico',
-    'Islandia', 'Israel', 'Estados Unidos', 'Zimbabwe'
+    'Islandia', 'Israel', 'Estados Unidos', 'Zimbabwe',
+    'Ucrânia', 'Belgica', 'Eslovaqui', 'Peru', 'Onduras'
 ]
 
 
 def deletar():
     texto = list(lista.curselection())
-    para_recursividade = 0  # Parar a volta da recursividade
-    for i, j in enumerate(texto):
-        if para_recursividade == 1:
-            break
-        try:
-            if j + 1 == texto[i + 1]:
-                continue
-            else:
-                para_recursividade = 1
-                lista.delete(texto[0], texto[i])
-                deletar()
-        except:
-            IndexError
-            lista.delete(texto[0], texto[-1])
+    decremento = 0
+    for i in texto:
+        lista.delete(i - decremento, i - decremento)
+        decremento += 1
 
 
 # cores
@@ -43,13 +34,12 @@ altura = 400
 posx = (root.winfo_screenwidth() - largura) / 2
 posy = (root.winfo_screenheight() - altura) / 2 - 15
 root.geometry('%dx%d+%d+%d' % (largura, altura, posx, posy))
-root.minsize(largura, altura)
-root.maxsize(largura, altura)
+root.resizable(0, 0)  # 0 = False | 1 = True
 
 # Widgets
 show = Label(root, text='Selecione um ou mais países',
              bg=fundo, fg=fonte, font=('Verdana', 14))
-show.pack(pady=5)
+show.pack(pady=5, padx=10)
 
 lista = Listbox(root, selectmode='multiple', bg=fundo_listbox,
                 highlightbackground='#42506b',
@@ -59,7 +49,7 @@ lista.pack(padx=10, expand=YES, fill='both')
 for i, item in enumerate(paises):
     num = i + 1
     num = '0' + str(num) if num < 10 else num
-    lista.insert(END,f' {num} - {item}')
+    lista.insert(END, f' {num} - {item}')
     lista.itemconfig(i, bg=cor_linha1)
 
 Button(root, text='Apagar', font='Verdana 10 bold',
